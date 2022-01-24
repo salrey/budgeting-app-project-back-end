05e0fe06b09bdcf3f8cb2b096d36ec0cd6022876
salrey <tekdehn@gmail.com>
2022-01-24 11:18:35 -0500

    Setup dummy data

---
+++ models/transactions.js
@@ -0,0 +1,98 @@
+const { v4 } = require('uuid');
+
+const transactions = [
+    {
+      id: "",
+      date: "April 1, 2019",
+      title: "Income",
+      amount: 1000,
+      from: "Employer",
+      category: "Income", 
+      type: "Income"
+    },
+    {
+      id: "",
+      date: "April 1, 2019",
+      title: "Taxes",
+      amount: -300,
+      from: "Accountant",
+      category: "Taxes",
+      type: "Expense"
+    },
+    {
+      id: "",
+      date: "April 1, 2019",
+      title: "Retirement",
+      amount: -200,
+      from: "Fidelity",
+      category: "Retirement",
+      type: "Expense"
+    },
+    {
+      id: "",
+      date: "April 1, 2019",
+      title: "Savings",
+      amount: -100,
+      from: "Savings Bank",
+      category: "Savings",
+      type: "Expense"
+    },
+    {
+      id: "",
+      date: "April 1, 2019",
+      title: "Credit Card Payment",
+      amount: -100,
+      from: "Discover",
+      category: "Credit Cards",
+      type: "Expense"
+    },
+    {
+      id: "",
+      date: "April 5, 2019",
+      title: "Monthly Birthday Money from Aunt Tilda",
+      amount: 20,
+      from: "Zelle",
+      category: "Transfer",
+      type: "Income"
+    },
+    {
+      id: "",
+      date: "April 5, 2019",
+      title: "Coffee",
+      amount: -4,
+      from: "Starbucks",
+      category: "Restaurants/Dining",
+      type: "Expense"
+    },
+    {
+      id: "",
+      date: "April 5, 2019",
+      title: "Internet",
+      amount: -100,
+      from: "Verizon",
+      category: "Bills/Utilities",
+      type: "Expense"
+    },
+    {
+      id: "",
+      date: "April 3, 2019",
+      title: "Groceries",
+      amount: -76,
+      from: "Deli",
+      category: "Groceries",
+      type: "Expense"
+    },
+    {
+      id: "",
+      date: "April 3, 2019",
+      title: "Pet Food",
+      amount: -7,
+      from: "Petco",
+      category: "Pets",
+      type: "Expense"
+    },
+  ];
+
+  transactions.forEach((transaction) => transaction.id = v4())
+
+  module.exports = transactions;
